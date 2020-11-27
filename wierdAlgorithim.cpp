2a99d43e86789c99263a3f91824c3f72c9d996c4
Gabbar010 <hk923458@gmail.com>
2020-11-27 18:32:16 +0530

    hello

---
+++ wierdAlgorithim.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	    long int n;
+	    cin >>n;
+	    cout<<n;
+	    while(n!=1){
+	        if(n%2!=0)
+	            n=3*n+1;
+	        else
+	           n=n/2;
+	       std::cout<<" " <<n ;}
+    	return 0;
+}
